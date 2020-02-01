770d436b3a4ee65c4ce12375f979eeedace1a525
nick-bigger <nick@heyhumming.com>
2020-01-31 20:34:27 -0800

    Added Slack request validation.

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 import os
 import requests
 import json
+import hmac
+import hashlib
+import base64
 
 from flask import Flask, jsonify, request
 
@@ -11,12 +14,31 @@ app = Flask(__name__)
 # hit the webhook with the message that was sent
 @app.route('/anon', methods=['POST'])
 def anon():
-    if 'ANONYMOUS_SLACK_WEBHOOK_URL' in os.environ:
+    if request_is_valid(request) and 'ANONYMOUS_SLACK_WEBHOOK_URL' in os.environ:
         data = {'text':request.form['text']}
         requests.post(url=os.environ['ANONYMOUS_SLACK_WEBHOOK_URL'], data=json.dumps(data))
-    
-    return '', 200
 
+        return '', 200
+
+    return '', 401
+
+
+def request_is_valid(request):
+    try:
+        request_body = request.get_data()
+        timestamp = request.headers['X-Slack-Request-Timestamp']
+        sig_basestring = 'v0:' + timestamp + ':' + request_body
+
+        slack_signature = request.headers['X-Slack-Signature']
+        slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+
+        dig = hmac.new(slack_signing_secret, msg=sig_basestring, digestmod=hashlib.sha256).digest() 
+        my_signature = 'v0=' + base64.b64encode(dig).decode()
+
+        return hmac.compare_digest(my_signature, slack_signature)
+
+    except:
+        return False
 
 if __name__ == '__main__':
     app.run()

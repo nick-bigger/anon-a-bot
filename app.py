e95010370edc429fd519259fe8bda94060b3d5e7
nick-bigger <nick@heyhumming.com>
2020-01-31 20:02:41 -0800

    Send an actual message through webhook.

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 #!flask/bin/python
+import os
+import requests
+import json
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 # Super simple. Slack pings with a request object,
-# respond with a 200 and the text that was sent
+# hit the webhook with the message that was sent
 @app.route('/anon', methods=['POST'])
 def anon():
-    return request.form['text'], 200
+    if 'ANONYMOUS_SLACK_WEBHOOK_URL' in os.environ:
+        data = {'text':request.form['text']}
+        requests.post(url=os.environ['ANONYMOUS_SLACK_WEBHOOK_URL'], data=json.dumps(data))
+    
+    return '', 200
 
 
 if __name__ == '__main__':

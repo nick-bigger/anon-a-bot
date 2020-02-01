c9ebfaed3a857c97c3f8cd2ec2ddf9ac8da9d5e2
nick-bigger <nick@heyhumming.com>
2020-01-31 22:06:19 -0800

    Added SlackEventsAPI

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 #!flask/bin/python
 import os
-import requests
-import json
 import hmac
-import hashlib
+import requests
 import base64
+import hashlib
+
+from slackeventsapi import SlackEventAdapter
+from slackclient import SlackClient
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+# Our app's Slack Event Adapter for receiving actions via the Events API
+slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
+
+# Create a SlackClient for your bot to use for Web API requests
+slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+slack_client = SlackClient(slack_bot_token)
+
+# Bind the Events API route to your existing Flask app
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events", app)
+
 
 # Super simple. Slack pings with a request object,
 # hit the webhook with the message that was sent
@@ -29,12 +43,28 @@ def request_is_valid(request):
     sig_basestring = ('v0:' + timestamp + ':' + request_body).encode("UTF-8")
 
     slack_signature = request.headers['X-Slack-Signature']
-    slack_signing_secret = bytes(os.environ['SLACK_SIGNING_SECRET'], encoding="UTF-8")
 
     dig = hmac.new(slack_signing_secret, msg=sig_basestring, digestmod=hashlib.sha256).digest() 
     my_signature = 'v0=' + base64.b64encode(dig).decode()
 
     return hmac.compare_digest(my_signature, slack_signature)
+
+
+# Example responder to greetings
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    message = event_data["event"]
+    # If the incoming message contains "hi", then respond with a "Hello" message
+    if message.get("subtype") is None and "hi" in message.get('text'):
+        channel = message["channel"]
+        message = "Hello <@%s>! :tada:" % message["user"]
+        slack_client.api_call("chat.postMessage", channel=channel, text=message)
+
+
+# Error events
+@slack_events_adapter.on("error")
+def error_handler(err):
+    print("ERROR: " + str(err))
 
 if __name__ == '__main__':
     app.run()
